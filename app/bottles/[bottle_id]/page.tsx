bd4e266757e47c51cd24de6058fed63b5966afad
Sam Hollenbeck <sam@hollenbecks.com>
2025-06-08 18:38:37 -0500

    Update css classes of text for BottlesPage and BottlePage

--- app/bottles/[bottle_id]/page.tsx
+++ app/bottles/[bottle_id]/page.tsx
@@ -22,9 +22,15 @@ export default async function BottlePage({
   if (!bottle) return notFound();
 
   return (
-    <div>
-      <h1>{bottle.name}</h1>
-      <p>{bottle.description}</p>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <p className="text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)] font-semibold">
+          {bottle.name}
+        </p>
+        <p className="text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          {bottle.description}
+        </p>
+      </main>
     </div>
   );
 }
