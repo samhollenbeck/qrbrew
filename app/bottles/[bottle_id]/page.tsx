092a6eb054b6e89905ddac77b3339fa6ea184cea
Sam Hollenbeck <sam@hollenbecks.com>
2025-06-08 18:44:45 -0500

    Center BottlePage text

--- app/bottles/[bottle_id]/page.tsx
+++ app/bottles/[bottle_id]/page.tsx
@@ -23,11 +23,11 @@ export default async function BottlePage({
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <p className="text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)] font-semibold">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center">
+        <p className="text-sm/6 text-center font-[family-name:var(--font-geist-mono)] font-semibold">
           {bottle.name}
         </p>
-        <p className="text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <p className="text-sm/6 text-center font-[family-name:var(--font-geist-mono)]">
           {bottle.description}
         </p>
       </main>
