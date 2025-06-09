36bfe1dcb4a513aae668c0bb341fc1381f87ae3d
Sam Hollenbeck <sam@hollenbecks.com>
2025-06-09 04:21:56 -0500

    Clean up BottlePage demo code

--- app/bottles/[slug]/page.tsx
+++ app/bottles/[slug]/page.tsx
@@ -2,16 +2,6 @@ export const dynamic = 'force-dynamic'
 
 import { notFound } from 'next/navigation';
 
-async function getBottleFromDB(id: string) {
-  const fakeBottleDB = [
-    { id: '00000000', name: 'Bottle #0', description: 'This is a slightly carbonated and sweet bottle of cider.' },
-    { id: '8asd5ga1', name: 'Bottle #1', description: 'This is a flat and sweet bottle of cider.' },
-    { id: '43nciq3d', name: 'Bottle #2', description: 'This is a highly carbonated and dry bottle of cider.' },
-  ];
-
-  return fakeBottleDB.find((bottle) => bottle.id === id) || null;
-}
-
 export default async function BottlePage({
   params,
 }: {
@@ -19,7 +9,7 @@ export default async function BottlePage({
 }) {
   const slug = (await params).slug;
 
-  const bottle = await getBottleFromDB(slug);
+  const bottle = {}
 
   if (!bottle) return notFound();
 
@@ -27,10 +17,10 @@ export default async function BottlePage({
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center">
         <p className="text-sm/6 text-center font-[family-name:var(--font-geist-mono)] font-semibold">
-          {bottle.name}
+          Name
         </p>
         <p className="text-sm/6 text-center font-[family-name:var(--font-geist-mono)]">
-          {bottle.description}
+          Description
         </p>
       </main>
     </div>
