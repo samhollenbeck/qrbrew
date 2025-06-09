24f9ee1252cc7d238e7937a4148d5dcc30a9bce0
Sam Hollenbeck <sam@hollenbecks.com>
2025-06-09 14:25:29 -0500

    Added table joins to getBottle and getBottles queries

--- lib/db/queries.ts
+++ lib/db/queries.ts
@@ -6,12 +6,80 @@ export async function getRecipes() {
 }
 
 export async function getBottles() {
-	const res = await pool.query('SELECT * FROM bottles')
+  const query =
+    `SELECT
+      bottles.id AS bottle_id,
+      bottles.slug,
+      bottles.bottledOn,
+      bottles.createdOn AS bottle_createdOn,
+
+      batches.id AS batch_id,
+      batches.name AS batch_name,
+      batches.descriptionFun,
+      batches.gravityFinal,
+      batches.startFermentOn,
+      batches.secondFermentOn,
+      batches.createdOn AS batch_createdOn,
+
+      recipes.id AS recipe_id,
+      recipes.name AS recipe_name,
+      recipes.description AS recipe_description,
+      recipes.expectedABV,
+      recipes.gravityOriginal,
+      recipes.urlPhoto AS recipe_photo,
+      recipes.createdOn AS recipe_createdOn,
+
+      -- Glassware
+      glasswareTypes.id AS glassware_id,
+      glasswareTypes.volume AS glassware_volume,
+      glasswareTypes.urlPhoto AS glassware_photo,
+      glasswareTypes.createdOn AS glassware_createdOn
+
+    FROM bottles
+    JOIN batches ON bottles.batchId = batches.id
+    JOIN recipes ON batches.recipeId = recipes.id
+    JOIN glasswareTypes ON bottles.glasswareTypeId = glasswareTypes.id
+    ORDER BY bottles.id;`
+	const res = await pool.query(query)
 	return res.rows
 }
 
 export async function getBottle(slug: string) {
-  const query = 'SELECT * FROM bottles WHERE slug = $1'
+  const query =
+    `SELECT
+      bottles.id AS bottle_id,
+      bottles.slug,
+      bottles.bottledOn,
+      bottles.createdOn AS bottle_createdOn,
+
+      batches.id AS batch_id,
+      batches.name AS batch_name,
+      batches.descriptionFun,
+      batches.gravityFinal,
+      batches.startFermentOn,
+      batches.secondFermentOn,
+      batches.createdOn AS batch_createdOn,
+
+      recipes.id AS recipe_id,
+      recipes.name AS recipe_name,
+      recipes.description AS recipe_description,
+      recipes.expectedABV,
+      recipes.gravityOriginal,
+      recipes.urlPhoto AS recipe_photo,
+      recipes.createdOn AS recipe_createdOn,
+
+      -- Glassware
+      glasswareTypes.id AS glassware_id,
+      glasswareTypes.volume AS glassware_volume,
+      glasswareTypes.urlPhoto AS glassware_photo,
+      glasswareTypes.createdOn AS glassware_createdOn
+
+    FROM bottles
+    JOIN batches ON bottles.batchId = batches.id
+    JOIN recipes ON batches.recipeId = recipes.id
+    JOIN glasswareTypes ON bottles.glasswareTypeId = glasswareTypes.id
+
+    WHERE bottles.slug = $1;`
   const values = [slug]
   const res = await pool.query(query, values)
   return res.rows[0]
