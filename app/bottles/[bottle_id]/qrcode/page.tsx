b50c372973abbffb663eab3b3852ce6c5b6b51ed
Sam Hollenbeck <sam@hollenbecks.com>
2025-06-09 03:13:59 -0500

    Add QRCode demo

---
+++ app/bottles/[bottle_id]/qrcode/page.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { useEffect } from 'react'
+import QRCode from 'qrcode'
+
+export default function QRPage() {
+	useEffect(() => {
+	  const generateAndDownloadQR = async () => {
+		const urlPrefix = 'https://brewery-app-phi.vercel.app/bottles/'
+		const dataUrl = await QRCode.toDataURL(urlPrefix, { margin: 1, scale: 10, errorCorrectionLevel: 'H' })
+
+		const link = document.createElement('a')
+		link.href = dataUrl
+		link.download = 'qr-code.png'
+		document.body.appendChild(link)
+		link.click()
+		document.body.removeChild(link)
+	  }
+
+	  generateAndDownloadQR()
+	}, [])
+
+	return (
+	  <div className="flex items-center justify-center h-screen">
+		<p>Generating your QR code and downloading...</p>
+	  </div>
+	)
+  }
