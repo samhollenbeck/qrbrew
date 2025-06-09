e5881ca73096676d9b7f0daa4642f15c123eb4db
Sam Hollenbeck <sam@hollenbecks.com>
2025-06-08 21:14:53 -0500

    Update HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
+import Link from 'next/link'
 
-export default async function Home() {
+export default async function HomePage() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
           Scan your bottle and learn everything you&apos;d like to know.
+          </li>
+          <li className="mb-2 tracking-[-.01em]">
+            See recipes <Link href="/recipes" className="underline">here</Link>.
           </li>
           <li className="tracking-[-.01em]">
             Enjoy responsibly and delightfully.
