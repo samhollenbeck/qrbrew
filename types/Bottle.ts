502388110b90320aa5c11d4eb8f1dc52d7fe4ef6
Sam Hollenbeck <sam@hollenbecks.com>
2025-06-10 19:46:22 -0500

    Added type Bottle

---
+++ types/Bottle.ts
@@ -0,0 +1,20 @@
+export type Bottle = {
+	// id: number
+	slug: string
+	// bottledOn: string | null
+	// createdOn: string
+	// batch: {
+	//   id: number
+	//   name: string
+	//   gravityFinal: number
+	//   recipe: {
+	// 	id: number
+	// 	name: string
+	//   }
+	// }
+	// glassware: {
+	//   id: number
+	//   volume: number
+	//   urlPhoto: string | null
+	// }
+}
