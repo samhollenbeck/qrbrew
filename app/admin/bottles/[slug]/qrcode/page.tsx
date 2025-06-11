e0617988a657dc9cadac66b2be71a5192caca573
Sam Hollenbeck <sam@hollenbecks.com>
2025-06-11 14:18:13 -0500

    Fix address

--- app/admin/bottles/[slug]/qrcode/page.tsx
+++ app/admin/bottles/[slug]/qrcode/page.tsx
@@ -1,4 +1,4 @@
-import QRPageClient from './QRPageClient'
+import QRPageClient from './qrpage-client'
 
 export default async function QRPage({ params }: { params: Promise<{ slug: string }> }) {
   const slug = (await params).slug
