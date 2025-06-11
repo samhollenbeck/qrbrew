fe3fa5865155c627b14bbb00b0298e90d89519fd
Sam Hollenbeck <sam@hollenbecks.com>
2025-06-11 04:57:42 -0500

    Added user authentication and authorization

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,40 @@
+import type { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from 'bcrypt';
+import { pool } from '@/lib/db/db';
+
+export const options: NextAuthOptions = {
+	providers: [
+		CredentialsProvider({
+			name: "Credentials",
+			credentials: {
+				username: {
+					label: "Username:",
+					type: "text",
+				},
+				password: {
+					label: "Password:",
+					type: "password",
+				}
+			},
+			async authorize(credentials) {
+				if (!credentials) return null;
+
+				const res = await pool.query(
+          			'SELECT * FROM users WHERE username = $1',
+          			[credentials.username]
+        		);
+        		const user = res.rows[0];
+
+				if (user && await bcrypt.compare(credentials.password, user.password)) {
+					// Remove password before returning user object
+
+					// eslint-disable-next-line @typescript-eslint/no-unused-vars
+					const { password: _, ...safeUser } = user;
+					return safeUser;
+				}
+				return null;
+			}
+		})
+	],
+}
---
+++ lib/db/auth.ts
@@ -0,0 +1,10 @@
+import { pool } from './db'
+import bcrypt from 'bcrypt'
+
+export async function registerUser(username: string, rawPassword: string) {
+  const hashedPassword = await bcrypt.hash(rawPassword, 12)
+  await pool.query(
+    'INSERT INTO users (username, password) VALUES ($1, $2)',
+    [username, hashedPassword]
+  )
+}
