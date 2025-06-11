037b6e18bcd2d2341360d57271253f03b7bc837c
Sam Hollenbeck <sam@hollenbecks.com>
2025-06-10 19:54:05 -0500

    Clean up Bottle type

--- types/Bottle.ts
+++ types/Bottle.ts
@@ -1,20 +1,3 @@
 export type Bottle = {
-	// id: number
 	slug: string
-	// bottledOn: string | null
-	// createdOn: string
-	// batch: {
-	//   id: number
-	//   name: string
-	//   gravityFinal: number
-	//   recipe: {
-	// 	id: number
-	// 	name: string
-	//   }
-	// }
-	// glassware: {
-	//   id: number
-	//   volume: number
-	//   urlPhoto: string | null
-	// }
 }
