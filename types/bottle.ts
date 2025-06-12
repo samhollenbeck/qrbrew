d184ac7fc73d12e64a5b6a8767c404b264970e50
HannahHollenbeck <68485238+HannahHollenbeck@users.noreply.github.com>
2025-06-11 23:43:57 -0500

    update Bottle type

--- types/bottle.ts
+++ types/bottle.ts
@@ -1,5 +1,8 @@
 export type Bottle = {
   slug: string
+  batch_name: string
+  recipe_name: string
+  descriptionfun: string
   sterilized: boolean
   glassware_volume: number
   citricAcidInGrams: number
